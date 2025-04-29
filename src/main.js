481cc043f7091da16d933b8b32854b8316bd1c84
MrSuricate <kevindu032270@gmail.com>
2025-04-29 09:40:52 +0200

    [GREEN] delimeter

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ export function add(numbers) {
         return 0;
     }
 
+    // Remplacer les retours à la ligne par des virgules
+    const normalizedInput = numbers.replace(/\n/g, ",");
+
     // Split la chaîne par les virgules pour obtenir un tableau de nombres
-    const numberArray = numbers.split(",");
+    const numberArray = normalizedInput.split(",");
 
     // Convertie chaque élément en nombre et les additionne
     return numberArray.reduce((sum, currentValue) => {
